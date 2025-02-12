dce5ec19acf6c9c5cbbd59c9f20681a3cf48be49
Luiz Fernando <aulas-programacao@bol.com.br>
2025-02-11 22:22:01 -0300

    flexv2

--- sw.js
+++ sw.js
@@ -8,14 +8,14 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('static')
             .then((cache) => {
-                cache.add('/flexV2/')
-                cache.add('/flexV2/index.html')
-                cache.add('/flexV2/style.css')
-                cache.add('/flexV2/app.js')
-                cache.add('/flexV2/imgflex.png')
-                cache.add('/flexV2/img/calcflex.png')
-                cache.add('/flexV2/img/etanol.png')
-                cache.add('/flexV2/img/gasolina.png')
+                cache.add('./flexV2/')
+                cache.add('./flexV2/index.html')
+                cache.add('./flexV2/style.css')
+                cache.add('./flexV2/app.js')
+                cache.add('./flexV2/imgflex.png')
+                cache.add('./flexV2/img/calcflex.png')
+                cache.add('./flexV2/img/etanol.png')
+                cache.add('./flexV2/img/gasolina.png')
  
             })
     )
